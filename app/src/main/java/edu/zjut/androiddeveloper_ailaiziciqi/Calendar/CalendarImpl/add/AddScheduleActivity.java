52d4cb29eb593df679256a9bbe65f5e0b8881efe
Cooper <mickeylyc@outlook.com>
2022-05-25 21:34:31 +0800

    更改了新建日程页面布局的名称。

--- app/src/main/java/edu/zjut/androiddeveloper_ailaiziciqi/Calendar/CalendarImpl/add/AddScheduleActivity.java
+++ app/src/main/java/edu/zjut/androiddeveloper_ailaiziciqi/Calendar/CalendarImpl/add/AddScheduleActivity.java
@@ -55,7 +55,7 @@ public class AddScheduleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.add_schedule_activity);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
